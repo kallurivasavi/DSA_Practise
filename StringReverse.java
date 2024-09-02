c254706a72f6108b7229e1bc31a290d28eb53bcc
kallurivasavi <vasavikalluri2@gmail.com>
2024-09-02 21:04:49 +0530

    To find Reverse of a Given String

--- StringReverse.java
+++ StringReverse.java
@@ -1,6 +1,10 @@
-String s="xyz";
-String ans="";
-for(int i=s.length()-1;i>=0;i--){
-     ans=ans+s.charAt(i);
+public class StringReverse{
+     public static void main(String args[]){
+          String s="xyz";
+          String ans="";
+          for(int i=s.length()-1;i>=0;i--){
+               ans=ans+s.charAt(i);
+          }
+          System.out.println(ans);
+     }
 }
-     System.out.println(ans);
