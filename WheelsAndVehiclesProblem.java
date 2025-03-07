1ca4f4c8bfebb07dd3cac4e61978546734d3d220
kallurivasavi <vasavikalluri2@gmail.com>
2025-03-07 06:14:48 +0530

    WheelsAndVehiclesProblem Program

--- WheelsAndVehiclesProblem.java
+++ WheelsAndVehiclesProblem.java
@@ -3,11 +3,11 @@ public class WheelsAndVehiclesProblem {
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int v=sc.nextInt();
-		int w=sc.nextInt();
-		int tw=((4*v)-w)/2;
-		int fw=v-tw;
-		System.out.println("TW:"+tw+" FW:"+fw);
+		int vehicle=sc.nextInt();
+		int wheels=sc.nextInt();
+		int TwoWheeler=((4*v)-w)/2;
+		int FourWheeler=v-tw;
+		System.out.println("TwoWheeler:"+TwoWheeler+" FourWheeler:"+FourWheeler);
 	}
 
 }
